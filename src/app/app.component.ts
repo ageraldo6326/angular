123fe89432e8b4d8e5118222ac0aa93c51a652d5
ageraldo6326 <49537283+ageraldo6326@users.noreply.github.com>
2025-05-03 15:11:38 -0400

    primera practica

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,40 @@
+import { DatePipe } from '@angular/common';
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 
+
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, DatePipe],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
   title = 'proyecto';
+  welcomeMessage = 'Bienvenido a mi aplicación Angular';
+
+  person = {
+    name: "Juan",
+    age: 30
+  }
+
+  num1:number = 12;
+  num2:number = 20;
+  resultado1 = this.num1 - this.num2;
+
+  num3 = 8;
+  num4 = 4;
+  resultado2 = this.num3 * this.num4;
+
+  radio:number = 5;
+
+  calculateArea (radio:number) {
+    return Math.PI * Math.pow(radio,2);
+  }
+
+
+  isLoggedIn:boolean = true;
+
+  currentDate:Date = new Date();
+
 }
